f2e2eb7280d8d11ba0bed4e689366509a053a40e
Tulja Kumawat <tuljakumawat@gmail.com>
2025-08-14 13:08:33 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const contactRouter = require('./routers/contactRouter')
+require('dotenv').config()
+
+app.use(cors({ origin: 'http://localhost:3000' || "Deploy URL " })); // adjust as needed
+app.use(express.json());
+
+
+
+app.use('/api/Contact', contactRouter)
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+    console.log(`server is running on PORT ${PORT}`));
---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const sendEmail = async ({ name, email, subject, message }) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: subject,
+        html: `
+      <h2>New Contact Message</h2>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong></p>
+      <p>${message}</p>
+    `
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
