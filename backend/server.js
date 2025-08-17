b284ab54989c4fe2a858afb55b420bd253a399c4
Tulja Kumawat <tuljakumawat@gmail.com>
2025-08-17 15:31:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,18 @@ const cors = require('cors')
 const contactRouter = require('./routers/contactRouter')
 require('dotenv').config()
 
-app.use(cors({ origin: 'http://localhost:3000' // "https://portfolio-frontend-jim0.onrender.com " })); // adjust as needed
+// app.use(cors({ origin: 'http://localhost:3000' // "https://portfolio-frontend-jim0.onrender.com " })); // adjust as needed
+// app.use(express.json());
+
+const allowedOrigins = [
+    'http://localhost:3000', // For local development
+    'https://portfolio-frontend-jim0.onrender.com' // For your deployed frontend
+];
+
+// Configure CORS to allow requests from the defined origins
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
+
 
 
 
